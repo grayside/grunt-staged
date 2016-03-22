875b598ef2a97d50da35c0207a5f3122bb8fbbe9
Jim Geurts <jim@biacreations.com>
2016-03-22 13:35:52 -0500

    Streamline vcs calls

--- test/vcs/a.js
+++ test/vcs/a.js
@@ -1,12 +1,12 @@
 'use strict';
 
 /**
- * Filter a list of file paths to return none.
+ * Filter a list of file paths to only return 'a' file paths.
  *
  * @param {function(Err, Array.<string>)} cb
  *     Callback called with any error and a list of files that are "staged".
  */
 var filterStagedPaths = exports.getStagedPaths = function(cb) {
-  cb(null, []);
+  cb(null, [ 'a', 'a2' ]);
 };
 
